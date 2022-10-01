c721f2cf769834c5d964a275f8eb72faa56c42a4
alvinotutu <alvinotutu@live.co.uk>
2022-10-01 18:08:17 +0200

    added ec2 deployment

--- main.tf
+++ main.tf
@@ -2,52 +2,160 @@ provider "aws" {
     region = "eu-west-1"
 }
 
-variable "cidr_blocks" {
-    description = "List of cidr blocks"
-    type = list(object({
-        cidr_block = string,
-        name = string}))
-}
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "avail_zone" {}
+variable env_prefix {}
+variable "my_ip" {}
+variable instance_type {}
+variable public_key_location {}
 
-variable "vpc_tag" {
-    description = "tag for VPC"
-    default = "my_vpc"
-}
-
-variable "subnet_tag" {
-    description = "tag for VPC"
-    default = "my_vpc_subnet"
-}
-
-
-resource "aws_vpc" "terraform-vpc" {
-    cidr_block = var.cidr_blocks[0].cidr_block
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
     tags = {
-        Name: var.cidr_blocks[0].name
+        Name: "${var.env_prefix}-vpc"
     }
 }
 
-resource "aws_subnet" "terraform-subnet-A" {
-    vpc_id = aws_vpc.terraform-vpc.id
-    cidr_block = var.cidr_blocks[1].cidr_block
-    availability_zone = "eu-west-1a"
+resource "aws_subnet" "myapp-subnet" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
     tags = {
-        Name: var.cidr_blocks[1].name
+        Name: "${var.env_prefix}-subnet-a"
     }
 }
 
-output "terraform-vpc-id" {
-    value = aws_vpc.terraform-vpc.id
+/* resource "aws_route_table" "myapp-route-table" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    route {
+        gateway_id = aws_internet_gateway.myapp-igw.id
+        cidr_block = "0.0.0.0/0"
+    }
+    tags = {
+        Name: "${var.env_prefix}-rtb"
+    }
+} */
+
+resource "aws_internet_gateway" "myapp-igw" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    tags = {
+        Name: "${var.env_prefix}-igw"
+    }
 }
 
-output "terraform-vpc-arn" {
-    value = aws_vpc.terraform-vpc.arn
+/* resource "aws_route_table_association" "myapp-subnet-rtb-assoc" {
+    subnet_id = aws_subnet.myapp-subnet.id
+    route_table_id = aws_route_table.myapp-route-table.id
+} */
+
+/* Use main route table created by AWS when we created this VPC*/
+resource "aws_default_route_table" "main-rtb" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+    tags = {
+        Name: "${var.env_prefix}-main-rtb"
+    }
 }
 
-output "terraform-subnet-A-id" {
-    value = aws_subnet.terraform-subnet-A.id
+/*
+resource "aws_security_group" "myapp-sg" {
+    name = "myapp-sg"
+    vpc_id = aws_vpc.myapp-vpc.id
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.my_ip]
+    }
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+    tags = {
+        Name: "${var.env_prefix}-myapp-sg"
+    }
+} */
+
+/* Use default security group created by AWS when we created this VPC*/
+
+resource "aws_default_security_group" "default-myapp-sg" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.my_ip]
+    }
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+    tags = {
+        Name: "${var.env_prefix}-default-sg"
+    }
 }
 
-output "terraform-subnet-A-arn" {
-    value = aws_subnet.terraform-subnet-A.arn
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+output "aws_ami_id" {
+    value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+output "aws_instance_ip" {
+    value = aws_instance.myapp-server.public_ip
+}
+
+resource "aws_key_pair" "ssh-key" {
+    key_name = "tf-created-key"
+    public_key = "${file(var.public_key_location)}"
+}
+
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id 
+    instance_type = var.instance_type
+    subnet_id = aws_subnet.myapp-subnet.id
+    vpc_security_group_ids = [aws_default_security_group.default-myapp-sg.id]
+    availability_zone = var.avail_zone
+
+    key_name = aws_key_pair.ssh-key.key_name 
+    associate_public_ip_address = true
+
+    user_data = file("entry_script.sh")
+
+    tags = {
+        Name: "${var.env_prefix}-server"
+    }
 }
